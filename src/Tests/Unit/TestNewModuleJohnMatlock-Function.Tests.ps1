48e35846e80199e26fc17b4d09f72982105ea82b
john-matlock-tech <60303867+john-matlock-tech@users.noreply.github.com>
2020-02-01 07:35:10 -0800

    Testing
    Testing

---
+++ src/Tests/Unit/TestNewModuleJohnMatlock-Function.Tests.ps1
@@ -0,0 +1,39 @@
+#-------------------------------------------------------------------------
+Set-Location -Path $PSScriptRoot
+#-------------------------------------------------------------------------
+$ModuleName = 'TestNewModuleJohnMatlock'
+$PathToManifest = [System.IO.Path]::Combine('..', '..', $ModuleName, "$ModuleName.psd1")
+#-------------------------------------------------------------------------
+if (Get-Module -Name $ModuleName -ErrorAction 'SilentlyContinue') {
+    #if the module is already in memory, remove it
+    Remove-Module -Name $ModuleName -Force
+}
+Import-Module $PathToManifest -Force
+#-------------------------------------------------------------------------
+$WarningPreference = "SilentlyContinue"
+#-------------------------------------------------------------------------
+#Import-Module $moduleNamePath -Force
+
+InModuleScope 'TestNewModuleJohnMatlock' {
+    #-------------------------------------------------------------------------
+    $WarningPreference = "SilentlyContinue"
+    #-------------------------------------------------------------------------
+    Describe 'TestNewModuleJohnMatlock Private Function Tests' -Tag Unit {
+        Context 'FunctionName' {
+            <#
+            It 'should ...' {
+
+            }#it
+            #>
+        }#context_FunctionName
+    }#describe_PrivateFunctions
+    Describe 'TestNewModuleJohnMatlock Public Function Tests' -Tag Unit {
+        Context 'FunctionName' {
+            <#
+                It 'should ...' {
+
+                }#it
+                #>
+        }#context_FunctionName
+    }#describe_testFunctions
+}#inModule
